c43c541659e7e109ae7c7ef47710d98f1be4338c
hanishbansal231 <hanishbansal803@gmail.com>
2024-06-27 23:09:52 +0530

    validation added

---
+++ server/src/utils/validation.ts
@@ -0,0 +1,10 @@
+export const isEmail = (email: string): boolean => {
+   const emailPattern =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return emailPattern.test(email);
+};
+
+export const isPassword = (password: string): boolean => {
+   const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+   return passwordPattern.test(password);
+};
